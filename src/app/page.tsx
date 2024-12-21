e293996c2377c559598cda1684f0d84e4d585967
yuchenfei <cf.yu@qq.com>
2024-12-21 11:22:45 +0800

    feat: update img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -238,7 +238,7 @@ export default function Example() {
       <div>
         <div className="carousel w-full">
           <div id="item1" className="carousel-item w-full">
-            <img src="/perfume.jpg" className="w-full" />
+            <img src="/door.jpg" className="w-full" />
           </div>
           <div id="item2" className="carousel-item w-full">
             <img src="/drone1.jpg" className="w-full" />
@@ -268,25 +268,25 @@ export default function Example() {
       <div>
         <div className="carousel rounded-box">
           <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="Burger" />
+            <img src="/007.jpg" alt="Burger" className="w-80" />
           </div>
           <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" alt="Burger" />
+            <img src="/351.jpg" alt="Burger" className="w-80" />
           </div>
           <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" alt="Burger" />
+            <img src="/350.jpg" alt="Burger" className="w-80" />
           </div>
           <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" alt="Burger" />
+            <img src="/349.jpg" alt="Burger" className="w-80" />
           </div>
           <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" alt="Burger" />
+            <img src="/348.jpg" alt="Burger" className="w-80" />
           </div>
           <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" alt="Burger" />
+            <img src="/347.jpg" alt="Burger" className="w-80" />
           </div>
           <div className="carousel-item">
-            <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" alt="Burger" />
+            <img src="/346.jpg" alt="Burger" className="w-80" />
           </div>
         </div>
       </div>
